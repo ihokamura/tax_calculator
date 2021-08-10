a8e3b44d0c4dc66056c880fd68d7cf3afd9f118c
ihokamura <ihokamura.contact@gmail.com>
2021-08-10 17:08:51 +0900

    Introduce fixture to set app property.

--- test_app.py
+++ test_app.py
@@ -1,15 +1,24 @@
 import json
+import pytest
 from sites.app import app
 
 app.config['TESTING'] = True
-app.DATA_DIRECTORY = 'test/data/'
-app.BOOK_FILE_PATH = app.DATA_DIRECTORY + 'book.csv'
-app.PAYMENTS_FILE_PATH = app.DATA_DIRECTORY + 'payments.txt'
-app.YEARS_FILE_PATH = app.DATA_DIRECTORY + 'years.txt'
 client = app.test_client()
 
 
-def test_get_years():
+@pytest.fixture
+def set_app_property():
+    def set_func(data_directory='test/data/', book_file_name='book.csv', payments_file_name='payments.txt', years_file_name='years.txt'):
+        app.DATA_DIRECTORY = data_directory
+        app.BOOK_FILE_PATH = data_directory + book_file_name
+        app.PAYMENTS_FILE_PATH = data_directory + payments_file_name
+        app.YEARS_FILE_PATH = data_directory + years_file_name
+
+    return set_func
+
+
+def test_get_years(set_app_property):
+    set_app_property()
     result = client.get('/years')
     test_text = '\n2020\n2021\n2022'
 
@@ -17,7 +26,8 @@ def test_get_years():
     assert result.status == '200 OK'
 
 
-def test_get_payments():
+def test_get_payments(set_app_property):
+    set_app_property()
     result = client.get('/payments')
     test_text = '\nテスト1\nテスト2\nテスト3'
 
@@ -25,7 +35,8 @@ def test_get_payments():
     assert result.status == '200 OK'
 
 
-def test_update_entries():
+def test_update_entries(set_app_property):
+    set_app_property(book_file_name='book_test_update.csv')
     test_data = [
         {'index': 0, 'date': '2020-01-01', 'name': 'テスト1', 'amount': 1000},
         {'index': 1, 'date': '2020-01-02', 'name': 'テスト2', 'amount': 2000},
@@ -37,7 +48,8 @@ def test_update_entries():
     assert result.status == '200 OK'
 
 
-def test_get_book_summary():
+def test_get_book_summary(set_app_property):
+    set_app_property(book_file_name='book_test_summary.csv')
     result = client.get('/book-summary')
     test_data = [
         {'amount': 2000, 'name': 'テスト1', 'year': '2020'},
